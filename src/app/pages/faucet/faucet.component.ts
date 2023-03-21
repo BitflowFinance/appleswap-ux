3c37e065ae4f75bed9cbfd31e529ab0360bcf1d9
martinsvinicius <vinicius.victor.sm@gmail.com>
2023-03-21 17:19:22 -0300

    faucet page

--- src/app/pages/faucet/faucet.component.ts
+++ src/app/pages/faucet/faucet.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { SelectItem } from 'src/app/shared/types/Select'
+import { userSession } from 'src/stacksUserSession'
 
 @Component({
   selector: 'app-faucet',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./faucet.component.scss']
 })
 export class FaucetComponent {
+  userSession = userSession
 
+  mintAmount = 0
+  tokenChoice?: SelectItem
+
+  tokenList: SelectItem[] = [
+    {
+      name: 'RED APPLES',
+      icon: 'red-apple.svg'
+    },
+    {
+      name: 'GREEN APPLES',
+      icon: 'green-apple.svg'
+    }
+  ]
+
+  constructor() {}
+
+  selectToken(event: SelectItem) {
+    this.tokenChoice = event
+    if (this.tokenChoice.name == 'RED APPLES') {
+      // TODO: implement this
+    } else if (this.tokenChoice.name == 'GREEN APPLES') {
+      // TODO: implement this
+    } else {
+      console.log('tokenChoice: ', this.tokenChoice)
+    }
+  }
+
+  mintTokens() {
+    // TODO: implement mint tokens
+  }
 }
